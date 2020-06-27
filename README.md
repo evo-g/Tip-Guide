21d9581154662b60bdb9aab791a8cad70b62773c
Evodio Garcia <evodiog@gmail.com>
2020-06-26 22:16:17 -0700

    Creates project using JS,CSS,HTML,Parcel. WIP

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tip Guide
+
+## Made to help those to easliy figure out a tip 
+- Basic JavaScript, HTML, CSS, and Parcel.js
+
+```
+npm i
+```
---
+++ src/main.js
@@ -0,0 +1,26 @@
+const bill = document.getElementById('bill');
+const tip = document.getElementById('tip');
+const calculatedTip = document.getElementById('calculatedTip');
+const calculatedTotal = document.getElementById('calculatedTotal');
+
+function calculateTotal(b, t) {
+  let total = +b + +t;
+  calculatedTotal.innerText = `Total: $ ${total.toFixed(2)}`;
+}
+
+function calculateTip(event) {
+  event.preventDefault()
+  const billValue = bill.value;
+  const tipValue = tip.value;
+  const tipTotal = billValue * tipValue;
+  calculatedTip.innerText = `Tip: $ ${tipTotal.toFixed(2)}`;
+  calculateTotal(billValue, tipTotal)
+}
+
+function clearForm() {
+  document.getElementById('tipForm').reset()
+}
+
+
+document.addEventListener('submit', calculateTip);
+document.addEventListener('onclick', clearForm)
